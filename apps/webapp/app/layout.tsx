368a3ad8feedd7c4bc75dd769b3731a10fb1770e
Fahad Ahmad <fahadahmad63862@gmail.com>
2023-09-30 23:08:51 +0530

    web app set up done with librabries i will likely use

--- apps/webapp/app/layout.tsx
+++ apps/webapp/app/layout.tsx
@@ -1,6 +1,8 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import { ClerkProvider } from "@clerk/nextjs";
+import TrpcProvider from "@/lib/trpc/Provider";
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +18,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+<ClerkProvider>
+<TrpcProvider>{children}</TrpcProvider>
+</ClerkProvider>
+</body>
     </html>
   )
 }
